70cce34bdfa6063dd8131691fd503822caaa03dd
AlvinLau-SchoolWork <chunhin.lau24@ycdsbk12.ca>
2025-05-26 10:24:04 -0400

    Completed ReverseAString

---
+++ src/recursion/ReverseAString.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class ReverseAString {
+    public static void reverse(String s) {
+        if (s.length() == 0) {
+            return;
+        }
+        System.out.print(s.charAt(s.length() - 1));
+        reverse(s.substring(0, s.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        reverse("hello");
+    }
+}
